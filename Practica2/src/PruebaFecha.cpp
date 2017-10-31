631677635a693505357878b1fbd8e9b15072b074
jmplz14 <jmplz14@correo.ugr.es>
2017-10-31 22:22:32 +0100

    Empiezo el main de pruebas de fecha historica

--- Practica2/src/PruebaFecha.cpp
+++ Practica2/src/PruebaFecha.cpp
@@ -1,7 +1,37 @@
 #include "Fecha_Historica.h"
 using namespace std;
 int main(int argc, char *argv[]){
+  cout << "--Constructores de la clase Fecha--" << endl;
 
+  cout << "Constructor por defecto" << endl;
+  Fecha_Historica por_defecto;
+  cout << por_defecto.to_s() << endl;
+
+  cout << "Constructor por fichero" << endl;
+  Fecha_Historica por_fichero(argv[1]);
+  cout << por_fichero.to_s() << endl;
+
+  cout << "Constructor Mediante string" << endl;
+  string cadena = "1900#Paso algo importante#Terminamos el proyecto de ED";
+  Fecha_Historica por_string(cadena);
+  cout << por_string.to_s() << endl;
+
+  cout <<"--Operadores--" << endl;
+
+  cout <<"Operador =" << endl;
+  Fecha_Historica operador_igual;
+  operador_igual = por_string;
+  cout << operador_igual.to_s() << endl;
+
+  cout <<"Operador <<" << endl;
+  cout << operador_igual << endl;
+
+  cout <<"Operador []" << endl;
+  cout << operador_igual[1] << endl;
   
+
+
+
+
   return 0;
 }
