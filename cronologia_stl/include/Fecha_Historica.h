2c17e52801a30f7c172e27c01b2c2153100a3434
jmplz14 <jmplz14@correo.ugr.es>
2017-12-02 02:35:23 +0100

    Empezada fecha_historica practica 4

---
+++ cronologia_stl/include/Fecha_Historica.h
@@ -0,0 +1,121 @@
+/**
+  * Clase Fecha_Historica. Representa los acontecimientos ocurridos en una fecha especificada.
+  @author     Heredia Castillo, Antonio Jesus
+  @author     Hinojosa Castro, Miguel Angel
+  @ author    Perez Lendinez, Jose Manuel
+  */
+#ifndef FECHA_HISTORICA_H
+#define FECHA_HISTORICA_H
+//#include <fstream>
+#include <string>
+#include <iostream>
+#include <utility>
+#include <set>
+#include <assert.h>
+
+
+class Fecha_Historica{
+  private:
+    const static char DELIMITADOR = '#';
+    std::pair< int,std::set<std::string> > fecha;
+
+    /*!Método Privado Auxiliar: mostrarErrorFormatoFecha
+            Función privada que muestra por pantalla el error que puede ocurrir con un string de formato Fecha_Historica.
+          */
+    void mostrarErrorFormatoFecha() const;
+
+    //!Método Privado Auxiliar: leerString
+      /*!
+        leerString permite procesar un string dado. Separa la cadena pasada por el delimitador (por defecto #) y utiliza las cadenas procesadas crear el objeto Fecha_Historica.
+        * \param cadena Cadena que va a ser procesada por la función.
+      */
+    bool leerString(std::string cadena);
+    /*!Método Privado Auxiliar: leerFichero
+          leerFichero lee y procesa un fichero. Hace uso de la función leeString para el procesado del fichero.
+          * \param fichero Fichero que va a ser usado por la función.
+		      * \return Retorna un booleano que nos indica si ha habido un error al leer el fichero (false) o si por el contrario, todo ha ido bien (true).
+      */
+    bool leerFichero(const char* fichero);
+  public:
+
+    Fecha_Historica();
+    /*!Construtor: Año nuevo
+          Constructor que asigna el año pasado por parámetro al objeto Fecha_Historica. Por defecto: num_acontecimientos = 0.
+		      * \param nuevo Año con el que se creará la nueva Fecha_Historica.
+      */
+      Fecha_Historica(int anio);
+    /*!Construtor: Lectura de String
+          Constructor que lee el String pasado por parámetro y lo procesa para crear un objeto de tipo Fecha_Historica.
+          * \param cadena String que contiene la cadena con el formato específico para crear una Fecha_Historica. Por defecto el formato es : año#acontecimiento1#...
+      */
+    Fecha_Historica(const std::string &cadena);
+    /*!Construtor: Lectura de fichero
+          Constructor que lee el fichero pasado por parámetro y lo procesa para crear un objeto de tipo Fecha_Historica.
+		      * \param direccion_fichero Direccion del fichero que contiene la información necesaria para crear un objeto tipo Fecha_Historica. Por defecto el formato es : año#acontecimiento1#...
+      */
+    Fecha_Historica(const char* direccion_fichero);
+
+    /*!Construtor: Constructor de Copia
+          Constructor de copia de la clase Fecha_Historica. Crea un objeto Fecha_Historica a partir de otro objeto Fecha_Historica.
+		      * \param Fecha_Historica Objeto que va a ser copiado por la función.
+      */
+    Fecha_Historica(const Fecha_Historica & f);
+    /*!Método: get
+        Función que devuelve el año de la Fecha_Historica.
+      */
+    int getAnio() const;
+    /*!Método: set
+          Función permite asignar un año nuevo a nuestro objeto Fecha_Historica.
+	        * \param nuevo Año nuevo para la Fecha_Historica. Se permiten años negativos (A.C.).
+      */
+    void cambiarAnio(int nuevo);
+    /*!Método: set
+          Función que permite añadir un nuevo acontecimiento a la Fecha_Historica.
+	        * \param cadena Cadena que contiene el nuevo acontecimiento.
+      */
+    void anadirAcontecimiento(const std::string& cadena);
+    /*!Método: eliminarAcontecimiento
+          Función que borra el acontecimiento que usar la posición indicada por el usuario. Acontecimiento 1 = posicion 1,...
+          * \param posicion Entero que indica la posición a borrar.
+      */
+    void eliminarAcontecimiento(int posicion);
+    /*!Método: eliminarAcontecimiento por string
+          Función que borra todas las ocurrencias con la cadena pasada por parámetro.
+          * \param cadena Cadena que se quiere borrar.
+      */
+    void eliminarAcontecimientos(const std::string& cadena);
+    /*!Método: get
+          Función que devuelve el número de acontecimientos de la Fecha_Historica.
+		      * \return numero de acontecimientos
+      */
+    int getNumeroAconteciemientos() const;
+    /*!Método: toString
+          Devuelve un string con un formato más legible para el usuario.
+	        * \return String que contiene la Fecha_Historica con un formato legible.
+      */
+    std::string to_s() const;
+    /*!Método: busqueda
+          Permite buscar una cadena dentro de todos los acontecimientos.
+	        * \param Cadena que queremos buscar dentro de los acontecimientos.
+		      * \return Set que contiene los resultados de la búsqueda.
+      */
+    //set<std::string> busqueda(const std::string &cadena) const;
+    /*!Método: get
+              Devuelve el delimitador usado.
+    		      * \return Devuelve el delimitador que es usado en los archivos. Por defecto #.
+          */
+    char getDelimitador() const;
+    /*!Método: Censurar Cadena
+      Función  que permite al usuario de la clase censurar una palabra dentro de los acontecimientos de un objeto Fecha_Historica. Por defecto cambia la palabra censurada por ****.
+      * \param Cadena Palabra que va a ser censurada.
+    */
+    //void censurarCadena(const std::string& Cadena);
+
+
+
+
+
+
+};
+
+#endif
---
+++ cronologia_stl/src/Fecha_Historica.cpp
@@ -0,0 +1,160 @@
+//Constructores
+#include "Fecha_Historica.h"
+#include <string>
+#include <iostream>
+#include <utility>
+#include <set>
+#include <fstream>
+
+
+using namespace std;
+//Constructores
+Fecha_Historica::Fecha_Historica(){
+  fecha.first = 0;
+}
+Fecha_Historica::Fecha_Historica(int anio){
+  fecha.first = anio;
+}
+
+Fecha_Historica::Fecha_Historica(const string &cadena){
+  bool creacion_de_clase = leerString(cadena);
+  assert(creacion_de_clase);
+}
+
+Fecha_Historica::Fecha_Historica(const char* direccion_fichero){
+  bool creacion_de_clase = leerFichero(direccion_fichero);
+  assert(creacion_de_clase);
+}
+
+Fecha_Historica::Fecha_Historica(const Fecha_Historica & f):fecha(f.fecha){
+}
+//operadores
+
+//Funciones privadas
+void Fecha_Historica::mostrarErrorFormatoFecha() const{
+  cerr << "¡¡Error en el formato de la fecha historica!!"<< endl;
+}
+
+bool Fecha_Historica::leerString(string cadena){
+
+  bool estado = true;
+  //buscamos la primerea posocion donde se encuentra el delimitador
+  size_t posicion = cadena.find(DELIMITADOR);
+  //se mira que almenos se contenga el delimitador una vez
+  if(posicion != cadena.npos ){
+
+    try
+    {
+      //nos quedamos con la primera parte delimitada del string y se alamacena
+      //anio
+      fecha.first = stoi(cadena.substr(0,posicion));
+
+      if(to_string(fecha.first) == cadena.substr(0,posicion)){
+        assert(to_string(fecha.first) == cadena.substr(0,posicion));
+        //se borra del string la parte que corresponde con el anio que es el primer
+        // token
+        cadena.erase(0,posicion+1);
+
+        //Se actualiza la nueva posocion del delimitador y se comprueba que no sea
+        //el final del string.
+        while( (posicion = cadena.find(DELIMITADOR)) != cadena.npos){
+
+          //se añade el acontecimiento hasta llegar al sigiente delimitador
+          anadirAcontecimiento(cadena.substr(0,posicion));
+
+          //se borra este aconteciento del string
+          cadena.erase(0,posicion+1);
+
+        }
+        //se añade el ultimo
+        anadirAcontecimiento(cadena.substr(0,posicion));
+    }else{
+      mostrarErrorFormatoFecha();
+      estado = false;
+    }
+    //excepcion generica cuando se da un error como por ejempos desbordamiento
+    //en el entero anio salta y pone el estado a false
+    }catch(...){
+        mostrarErrorFormatoFecha();
+        estado = false;
+    }
+  }else{
+    mostrarErrorFormatoFecha();
+    estado = false;
+  }
+
+  return estado;
+}
+
+bool Fecha_Historica::leerFichero(const char* direccion_fichero){
+  bool estado = true;
+
+  string linea;
+
+  ifstream fichero;
+  fichero.open(direccion_fichero);
+
+  if(fichero){
+
+    getline(fichero,linea);
+    estado = leerString(linea);
+    fichero.close();
+
+  }else{
+    cerr << "¡¡Error al acceder al fichero "<< direccion_fichero << "!!" << endl;
+    estado = false;
+  }
+
+  return estado;
+}
+
+//Publicas
+int Fecha_Historica::getAnio() const {
+  return fecha.first;
+}
+
+void Fecha_Historica::anadirAcontecimiento(const string& cadena){
+  fecha.second.insert(cadena);
+}
+
+void Fecha_Historica::cambiarAnio(int nuevo){
+  fecha.first = nuevo;
+}
+
+void Fecha_Historica::eliminarAcontecimiento(int posicion){
+    assert(posicion < static_cast<int>(fecha.second.size()) && posicion >= 0);
+    set<string>::iterator it = fecha.second.begin();
+    for(int i = 0; i < posicion; i++)
+      ++it;
+
+    fecha.second.erase(it);
+}
+
+void Fecha_Historica::eliminarAcontecimientos(const string& cadena){
+  set<string>::iterator it = fecha.second.begin();
+  while(it != fecha.second.end()){
+    if( it->find(cadena) != std::string::npos)
+      it = fecha.second.erase(it);
+    else
+      ++it;
+
+  }
+}
+
+int Fecha_Historica::getNumeroAconteciemientos() const{
+  return static_cast<int>(fecha.second.size());
+}
+
+std::string Fecha_Historica::to_s() const{
+  string s;
+  s += "Año: " + to_string(fecha.first) + "\n";
+  int contador_acontecimientos = 0;
+
+  for (set<string>::iterator it = fecha.second.begin() ; it != fecha.second.end(); ++it){
+    s += "Acontecimiento " + to_string(contador_acontecimientos) + ": " + *it + "\n";
+    contador_acontecimientos++;
+  }
+
+  s += "\n";
+  return s;
+}
